e2e1e34ba584d1050b751f06d2d8e85a1fbd1783
Ian <iantang9000@gmail.com>
2024-02-22 22:54:02 -0500

    reformatted second floor, added change floor functionality

--- src/ImagePanel.java
+++ src/ImagePanel.java
@@ -1,38 +1,52 @@
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class ImagePanel extends JPanel{
-	private static BufferedImage map1, map2, currentMap;
+public class ImagePanel extends JPanel {
+    private static BufferedImage map1, map2, currentMap;
+    
+    public ImagePanel() {
+        super();
+        try {
+            map1 = ImageIO.read(new File("./res/first_floor.png"));
+            map2 = ImageIO.read(new File("./res/second_floor.png"));
+            currentMap = map1; // Default map
+        } catch (IOException e) {
+            e.printStackTrace();
+        }   
+    }
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (currentMap != null) {
+            g.drawImage(currentMap, 0, 0, this.getWidth(), this.getHeight(), this);
+        }
+    }
+    
+    public void setCurrentMap(int n) {
+        if (n == 1) {
+            currentMap = map1;
+        } else {
+            currentMap = map2;
+        }
+        repaint();//repaint after changing the map
+    }
+    public void placeMarker(int x, int y) {
+        // Placeholder for placing a marker at the specified location
+        // This could involve updating the map image to include a marker icon at the specified coordinates
+        System.out.println("Placing marker at: " + x + ", " + y);
+        Graphics2D g = currentMap.createGraphics();
+        g.drawRect(x, y, 10, 10);
+        repaint();
+    }
 	
-	public ImagePanel() {
-		super();
-		try {
-			map1 = ImageIO.read(new File("./res/first_floor.png"));
-			map2 = ImageIO.read(new File("./res/second_floor-1.png"));
-			currentMap = map1;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}	
-		
-	}
-	
-	public void paintComponent(Graphics g) {
-	    super.paintComponent(g);
-	    g.drawImage(currentMap, 0, 0, this);
-	    repaint();
-	}
-	
-	public void setCurrentMap(int n) {
-		if(n ==1)currentMap = map1;
-		else
-			currentMap = map2;
-		repaint();
-	}
-	
+    public BufferedImage getCurrentMap() {
+    	return currentMap;
+    }
 }
--- src/Map.java
+++ src/Map.java
@@ -1,43 +1,61 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.JOptionPane;
 
-public class Map extends JFrame {
-	private BufferedImage map1, map2;
+public class Map extends JFrame implements ActionListener, MouseListener {
+	private BufferedImage map1, map2, currentMap;
 	private JLabel backgroundLabel;
 	private int currentFloor = 1;
 	private JButton back, changeFloor;
 	private ImagePanel map;
 	private Font textFont = new Font("Verdana", Font.BOLD, 14);
 	public Map() {
-		map = new ImagePanel();
-		changeFloor = new JButton("Change Map");
-		changeFloor.setBounds(1600, 300, 300, 200);
-		backgroundLabel = new JLabel();
-		backgroundLabel.add(changeFloor);
-		map.add(changeFloor);
-		add(map);
-		setSize(1920, 1080);
-		setVisible(true);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLocationRelativeTo(null);
+		loadMaps();
+		initUI();
 	}
+	
+	private void initUI() {
+        map = new ImagePanel(); // Assuming the first floor is the default
+        changeFloor = new JButton("Change Map");
+        changeFloor.setBounds(1600, 300, 300, 50); // Adjust size as needed
+        changeFloor.addActionListener(this);
 
+        // Adding button directly to the map panel for simplicity
+        map.setLayout(new BorderLayout());
+        map.add(changeFloor, BorderLayout.SOUTH); // Example placement
+
+        add(map);
+        setSize(1920, 1080);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+	public void makeButton(JButton b) {
+		b.addActionListener(this);
+		b.setFocusable(false);	
+		b.setForeground(Color.black);
+		b.setBackground(Color.orange);
+		backgroundLabel.add(b);
+	}
+	
 	private void loadMaps() {
         try {
             map1 = ImageIO.read(new File("./res/first_floor.png"));
-            map2 = ImageIO.read(new File("./res/second_floor-1.png"));
+            map2 = ImageIO.read(new File("./res/second_floor.png"));
+            currentMap = map1;
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -45,7 +63,8 @@ public class Map extends JFrame {
 	
 	private void changeFloor() {
         currentFloor = 3 - currentFloor;
-        ((ImagePanel) map).setCurrentMap(currentFloor);
+        map.setCurrentMap(currentFloor);
+        currentMap = map.getCurrentMap();
     }
 
 	private void saveFloor() {
@@ -59,5 +78,37 @@ public class Map extends JFrame {
 		Map m = new Map();
 		
 	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == changeFloor) {
+			changeFloor();
+		}
+	}
+	
+	
+	public void mouseClicked(MouseEvent e) {
+        // Call displayChoices and pass the mouse click coordinates
+        
+    }
+
+    public void displayChoices(int x, int y) {
+        // Prompt the user to confirm if they want to place a marker
+        int response = JOptionPane.showConfirmDialog(this, "Do you want to place a marker at this location?", "Confirm", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+        
+        if (response == JOptionPane.YES_OPTION) {
+            map.placeMarker(x, y);
+        }
+    }
+
+	public void mousePressed(MouseEvent e) {		
+		displayChoices(e.getX(), e.getY());
+	}
+	public void mouseReleased(MouseEvent e) {		
+	}
+	public void mouseEntered(MouseEvent e) {		
+	}
+	public void mouseExited(MouseEvent e) {		
+	}
+	
 	
 }
